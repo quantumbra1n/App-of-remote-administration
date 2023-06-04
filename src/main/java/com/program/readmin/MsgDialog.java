1d7c126c24523e2de05933240f4d35804f3f4b5c
Ivan Byzov <byzovivan03@mail.ru>
2023-06-04 21:32:12 +0500

    Base class for dialogs

--- src/main/java/com/program/readmin/MsgDialog.java
+++ src/main/java/com/program/readmin/MsgDialog.java
@@ -2,20 +2,32 @@ package com.program.readmin;
 
 import javafx.scene.control.Alert;
 
-public class MsgDialog {
-    // Абстрактный класс диалоговых окон
-    abstract class AbstractMsgDialog {
-        protected Alert alert; // Тип диалогового окна
-        protected String title; // Название диалогового окна
-        protected String contentText; // Текстовое сообщение
+// Абстрактный класс диалоговых окон
+abstract class AbstractMsgDialog {
+    protected Alert alert; // Тип диалогового окна
+    protected String title; // Название диалогового окна
+    protected String contentText; // Текстовое сообщение
 
-        // Конструктор абстрактного класса
-        public AbstractMsgDialog(Alert alert, String title, String contentText) {
-            this.alert = alert;
-            this.title = title;
-            this.contentText = contentText;
-        }
+    // Конструктор абстрактного класса
+    public AbstractMsgDialog(Alert alert, String title, String contentText) {
+        this.alert = alert;
+        this.title = title;
+        this.contentText = contentText;
+    }
 
-        public abstract void show();
+    public abstract void show();
+}
+
+public class MsgDialog extends AbstractMsgDialog{
+    public MsgDialog(Alert alert, String title, String contentText) {
+        super(alert, title, contentText);
+    }
+
+    @Override
+    public void show() {
+        this.alert.setTitle(this.title);
+        this.alert.setHeaderText(null);
+        this.alert.setContentText(this.contentText);
+        this.alert.showAndWait();
     }
 }
