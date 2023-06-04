d652f7bf64a2e635d1ee15b5f4dc82bb804366e0
Ivan Byzov <byzovivan03@mail.ru>
2023-06-04 21:13:16 +0500

    Create attributes for connecting to DB

--- src/main/java/com/program/readmin/DataBaseConnector.java
+++ src/main/java/com/program/readmin/DataBaseConnector.java
@@ -1,4 +1,13 @@
 package com.program.readmin;
 
+import java.sql.Connection;
+import java.sql.Statement;
+
 public class DataBaseConnector {
+    public static final String USER_NAME = "root";
+    public static final String PASSWORD = "1234";
+    public static final String URL = "jdbc:mysql://localhost:3306/remadmin_db";
+    public static final String DRIVER = "com.mysql.cj.jdbc.Driver"; // Коннектор подключен как библиотека в project structure -> sdk -> java 18
+    public static Statement statement;
+    public static Connection connection;
 }
