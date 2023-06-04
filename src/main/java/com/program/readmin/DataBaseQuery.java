9050208bae172739e1fde395caead4750f823ab9
Ivan Byzov <byzovivan03@mail.ru>
2023-06-04 21:26:07 +0500

    Create attributes for query

--- src/main/java/com/program/readmin/DataBaseQuery.java
+++ src/main/java/com/program/readmin/DataBaseQuery.java
@@ -1,5 +1,7 @@
 package com.program.readmin;
 
 public class DataBaseQuery {
-    
+    private String queryTruncateTable = "TRUNCATE TABLE users;";
+    private String querySelectAllUsers = "SELECT * FROM users;";
+    private  String queryInsertIntoUser = "INSERT INTO users (id, email, login, password, role) VALUES (";
 }
