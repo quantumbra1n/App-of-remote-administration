ac50f7a619457dfc3a157d2106cdfd11ab181d00
Ivan Byzov <byzovivan03@mail.ru>
2023-06-04 21:19:00 +0500

    Import Panes

--- src/main/java/com/program/readmin/HelloController.java
+++ src/main/java/com/program/readmin/HelloController.java
@@ -21,4 +21,26 @@ import java.util.Objects;
 import static com.program.readmin.DataBaseConnector.*;
 
 public class HelloController {
+    // ПАНЕЛЬ МЕНЮ
+    @FXML
+    private MenuBar menuBar;
+
+    @FXML
+    private Menu userNameMenu;
+
+    @FXML
+    private MenuItem dbMenuItem;
+
+    // ПАНЕЛИ
+    @FXML
+    private Pane authorizationPane; // Панель авторизации
+
+    @FXML
+    private Pane registrationPane; // Панель регистрации
+
+    @FXML
+    private Pane actionPane; // Панель выбора действия
+
+    @FXML
+    private Pane typeConnectionPane; // Панель выбора типа подключения
 }
