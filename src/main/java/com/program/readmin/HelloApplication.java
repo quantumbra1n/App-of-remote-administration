8ee239cc3d4bbed019d5584d509a212064a5f85d
Ivan Byzov <byzovivan03@mail.ru>
2023-06-04 21:07:31 +0500

    Set title and icon of program

--- src/main/java/com/program/readmin/HelloApplication.java
+++ src/main/java/com/program/readmin/HelloApplication.java
@@ -13,11 +13,11 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("remadmin.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        //stage.setTitle("Программа удалённого администрирования");
+        stage.setTitle("Программа удалённого администрирования");
         stage.setScene(scene);
         stage.centerOnScreen();
         stage.setResizable(false);
-        //stage.getIcons().add(new Image("C:\\RemAdmin\\pc.png"));
+        stage.getIcons().add(new Image("C:\\RemAdmin\\pc.png"));
         stage.show();
     }
     public static void main(String[] args) {
